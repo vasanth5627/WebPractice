364ecf0d5376ded93f5404a48cc7f86e6385702b
vasanthkumar <chicvasanth@gmail.com>
2023-01-19 15:48:57 +0530

    new changes

--- Documents/Work/Javascript/DynamicElements/script.js
+++ Documents/Work/Javascript/DynamicElements/script.js
@@ -19,11 +19,18 @@ let customers = ["Jhon","Mike","Vasanth"];
 // custEle.classList.add('customer','test');
 // custEle.classList.remove('customer','test');
 
+const render = (custEle)=>{
+    custEle.classList.add('render');
+    
+}
+
 const renderCustomers = function(value,index){
     const custEle = document.createElement('div');
     custEle.textContent = value;
     ele.append(custEle);
     custEle.classList.add('customer','test');
+     setTimeout(render,2000,custEle)
+   
 }
 customers.forEach(renderCustomers);
 
